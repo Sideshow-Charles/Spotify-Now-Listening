9c07bc7c67f9e03fd0fc805b3a9b2a3b025165ca
charles144 <charles@flutterwavego.com>
2024-10-12 14:24:10 +0100

    installed SCSS and basic setup

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,9 @@
 <template>
-  <Tutorial/>
+ <div>
+  <p class="song__name">PRADA</p>
+  <p class="artiste__name">AKA, KHULI CHANA</p>
+  <p class="album__name">MASS COUNTRY</p>
+ </div>
 </template>
 
 <script lang="ts">
