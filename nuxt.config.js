d26b04a1612960f28f5be73fe13d600795c53c31
sideshow-charles <charlesonuoha97@gmail.com>
2024-10-25 18:03:35 +0100

    add installed packages to nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -36,9 +36,14 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
+    '@nuxtjs/axios',
+    '@nuxtjs/dotenv'
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-  }
+  },
+
 }
+
+
