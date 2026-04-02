b62e6722d368222739b95c45bb41e03603dad409
sideshow-charles <charlesonuoha97@gmail.com>
2026-04-02 16:28:02 +0100

    update path

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,7 +22,7 @@ export default {
   ],
 
   serverMiddleware: [
-    { path: '/api', handler: require('./server-middleware/spotify.js') }
+    { path: '/api', handler: path.resolve(__dirname, 'server-middleware/spotify.js') }
   ],
 
 
