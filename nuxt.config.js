841ff3bc6968095d23b0dfe99166bd69dba66df5
sideshow-charles <charlesonuoha97@gmail.com>
2026-04-02 16:09:40 +0100

    Remove dotenv

--- nuxt.config.js
+++ nuxt.config.js
@@ -42,12 +42,13 @@ export default {
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     '@nuxtjs/axios',
-    '@nuxtjs/dotenv'
+    // '@nuxtjs/dotenv'
   ],
   env: {
     SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
     SPOTIFY_CLIENT_SECRET: process.env.SPOTIFY_CLIENT_SECRET,
-    SPOTIFY_REDIRECT_URI: process.env.SPOTIFY_REDIRECT_URI
+    SPOTIFY_REDIRECT_URI: process.env.SPOTIFY_REDIRECT_URI,
+    SPOTIFY_REFRESH_TOKEN: process.env.SPOTIFY_REFRESH_TOKEN
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
