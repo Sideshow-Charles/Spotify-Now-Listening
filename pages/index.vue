5c655cd0c8f5a020661ac490383445c6295bdf5e
sideshow-charles <charlesonuoha97@gmail.com>
2026-06-06 13:56:46 +0100

    Feat(Loader): Use setTimeout function to display loader text longer

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,16 @@
 <template>
- <div class="app">
-   <p class="base__text">Hang On, Let's Get While We Get What Charles Is Listening To</p>
- </div>
+  <div class="app">
+    <p class="base__text">Hang On, Let's Get While We Get What Charles Is Listening To</p>
+  </div>
 </template>
 
 
 <script>
 export default {
   mounted() {
-    this.$router.push('/now-playing');
+    setTimeout(() => {
+      this.$router.push('/now-playing');
+    }, 5000);
   },
 };
 </script>
